538ed1820e228dc96b5ab03e5f2ed539c1ab2ca4
laeshe1886 <larsvoegtle@gmx.ch>
2024-03-04 00:57:51 +0100

    Aufgabe 4

--- src/main/java/ch/hslu/ad/sw01/aufgabe2/Laufzeit.java
+++ src/main/java/ch/hslu/ad/sw01/aufgabe2/Laufzeit.java
@@ -11,9 +11,14 @@ public class Laufzeit {
 
 
     public static void task(final int n) {
-        task1(); task1(); task1(); task1(); // T ~ 4
+        task1();
+        task1();
+        task1();
+        task1(); // T ~ 4
         for (int i = 0; i < n; i++) { // äussere Schleife: n-mal
-            task2(); task2(); task2(); // T ~ n · 3
+            task2();
+            task2();
+            task2(); // T ~ n · 3
             for (int j = 0; j < n; j++) { // innerer Schleife: n-mal
                 task3(); task3(); // T ~ n · n· 2
             }
@@ -21,15 +26,15 @@ public class Laufzeit {
         LOG.info("{} function calls were made for n = {}", callCount, n);
     }
 
-    public static void task1(){
+    private static void task1(){
         sleepForTime(5);
         addCallCount();
     }
-    public static void task2(){
+    private static void task2(){
         sleepForTime(5);
         addCallCount();
     }
-    public static void task3(){
+    private static void task3(){
         sleepForTime(5);
         addCallCount();
     }
